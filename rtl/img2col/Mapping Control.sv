d973a3d48bceeb84c22814a51b11667e8ac490df
Raneem Alaa <raneemalaa55@gmail.com>
2021-12-13 15:50:37 +0200

    counters fix

--- rtl/img2col/Mapping Control.sv
+++ rtl/img2col/Mapping Control.sv
@@ -1,84 +1,108 @@
 module Map_Control(
-input logic clk,nrst,start,
-output logic [5:0] round,PU1_add,PU_No,row_No
+  input logic clk,nrst,start,
+  output logic [5:0] current_round,current_PU1_add,current_PU_No,current_row_No
 );
 
 
-typedef enum logic [1:0]{ 
-Idle =2'b00,
-Buffering =2'b01,
-Working =2'b10}states;
-states current_state,next_state; // *** 
+  enum logic [1:0]{ Idle      =2'b00,
+                    Buffering =2'b01,
+                    Working   =2'b10 }  next_state,current_state;
 
+logic [5:0] next_PU1_add, next_PU_No, next_row_No, next_round ;
+  always_ff @(posedge clk, negedge nrst)
+  begin
+    if(!nrst)begin
+      current_state <= Idle;
+      current_PU1_add <=0;
+      current_PU_No <=0; 
+      current_row_No <=0; 
+      current_round <=0;      
+    end
+    else
+    begin
+      current_state   <= next_state;
+      current_PU1_add <= next_PU1_add;
+      current_PU_No   <= next_PU_No;
+      current_row_No  <= next_row_No;
+      current_round   <= next_round;
+    end
+  end
 
-always_ff @(posedge clk, negedge nrst)
-begin
-if(!nrst)begin
-  current_state <= Idle;
-        
-end
-else
-  current_state <= next_state;
-end
+  always_comb
+  begin
+    next_state = current_state;
+    next_PU1_add= current_PU1_add;
+    next_PU_No=current_PU_No;
+    next_row_No=current_row_No;
+    next_round=current_round;
+    unique case(current_state)
+      Idle:
+      begin
+        next_PU1_add = 0;
+        next_PU_No   = 0;
+        next_row_No  = 0;
+        next_round   = 0;
+        if(start)
+        begin
+          next_state = Buffering;
+        end
+        else
+        begin
+          next_state = current_state;
+        end
+      end
 
-always_comb
-begin
-  case(current_state)
-     Idle:begin
-        PU1_add=0;
-        PU_No=0;
-        row_No=0;
-        round=0;
-          if(start)
-            next_state=Buffering;
-          else
-            next_state=current_state;
-end
-Buffering:
+      Buffering:
+      begin
+        if( next_PU1_add < 6'd04 )
+        begin
+          next_PU1_add = next_PU1_add + 1;
+          next_state = Buffering;
+        end
+        else
+        begin
+          if( next_PU_No == 5'd27)
+            begin
+              next_PU_No   = 0;
+              next_PU1_add = 0;
+              next_row_No  = next_row_No + 1;
+		          next_state=current_state;
+            end
+          else 
           begin
-          if(PU1_add==5'd04 )begin
-             PU_No=PU_No+1;
-               if(row_No==4'd04)begin
-                  next_state=Working;
-                  round=0;
-                  end
-               else
-                  next_state=current_state;
-               if(PU_No==5'd27)begin
-                  PU_No=0;
-                  PU1_add=0;
-                  row_No=row_No+1;
-		  next_state=current_state; // ***
-		 end
-               else begin
-		next_state=current_state; // ***
-		 end
-               end
-          else begin
-          PU1_add=PU1_add+1;
-	  next_state=current_state;  // ***  
+            next_PU_No = next_PU_No + 1;
+            if( next_row_No == 4'd04)
+            begin
+            next_state = Working;
+            next_round = 0;
+            end
+            else
+            begin
+              next_state = current_state;
+            end
           end
-         
-end
+        end
+      end
+
 Working:
 begin
-           row_No =4'd04;
+           next_row_No =4'd04;
         
-           if(PU1_add==5'd04 )begin
-           PU_No=PU_No+1;
-               if(PU_No==5'd27)
+           if(next_PU1_add==5'd04 )begin
+           next_PU_No=next_PU_No+1;
+               if(next_PU_No==5'd27)
                begin
-               round=round+1;
-               PU_No=0;
-               PU1_add=0;
-               if(round==27)
+               next_round=next_round+1;
+               next_PU_No=0;
+               next_PU1_add=0;
+               if(next_round==27)
                next_state=Idle;
                else
                next_state=current_state  ;    
                end  
            end 
            else
-           PU1_add=PU1_add+1; 
+           next_PU1_add=next_PU1_add+1; 
 	   next_state=current_state; // ***
 
 end
