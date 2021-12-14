d2a6c4e097e77fb9cc586043b9c739caebe66cb8
youssef mehdawy <mohamedmehdawy@yahoo.com>
2021-12-14 21:34:44 +0200

    updated files

---
+++ rtl/Pooling/pooling_top.sv
@@ -0,0 +1,32 @@
+module max_avg_pooling
+  #(  parameter data_width = 8)
+   (   
+       input clk,nrst,start,en,
+       input [datawidth-1:0]sys_out,pooling_out,
+       output [datawidth-1:0] out);
+
+    logic [datawidth-1:0] x,z;
+
+    assign x =(get_this_from_control1)?sys_out:out;
+
+
+    regfile Register_File(
+        .clk(clk)
+        .nrst(nrst)
+        .Wr_ctrl(get_this_from_control2)
+        .in1(sys_out)
+        .in2(out)
+        .add_in1(get_this_from_control3)
+        .add_in2(get_this_from_control4)
+        .add_out(get_this_from_control5)
+        .out(z)
+    )   
+
+
+    pooling max_avg_pooling (
+        .in1(x)
+        .in2(z)
+        .en(en)
+        .out(out)
+    )
+endmodule
--- rtl/Pooling/regfile.sv
+++ rtl/Pooling/regfile.sv
@@ -6,11 +6,11 @@ module Register_File
     input logic clk, nrst, Wr_ctrl,
     input logic [data_width-1:0] in1,in2,
     input logic [3:0] add_out,add_in1, add_in2, //address bus
-    output logic [data_width-1:0] out1, out2, out3, out4
+    output logic [data_width-1:0] out
   );
   logic [data_width-1:0] registers [15:0];
 
-  assign out1 = registers[add_out];
+  assign out = registers[add_out];
 
   always @ (posedge clk, negedge nrst) 
   begin
