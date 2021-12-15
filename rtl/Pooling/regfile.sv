81bb6997923d5c29ea52d1deb70792eacba050df
marwan ibrahim zaki <50636605+marwanibrahim546@users.noreply.github.com>
2021-12-15 21:55:15 +0200

    add read

--- rtl/Pooling/regfile.sv
+++ rtl/Pooling/regfile.sv
@@ -1,20 +1,30 @@
-module Register_File
-  #(
-     parameter data_width = 8
-   )
-  (
-    input logic clk, nrst, Wr_ctrl,
-    input logic [data_width-1:0] in1,in2,
-    input logic [3:0] add_out,add_in1, add_in2, //address bus
+module regfilePooling#(
+    parameter data_width = 16 ,
+    reg_num = 16 , 
+    address_num = 4 
+)(
+    input  logic clk, nrst, wr_ctrl,
+    input  logic [data_width-1:0] in1,in2 , 
+    input  logic [address_num-1:0] adrs_in1, adrs_in2 , adrs_out , 
     output logic [data_width-1:0] out
-  );
-  logic [data_width-1:0] registers [15:0];
+);
 
-  assign out = registers[add_out];
 
-  always @ (posedge clk, negedge nrst) 
+logic [data_width-1:0] registers [reg_num-1:0];
+
+always_ff @ (posedge clk, negedge nrst) 
   begin
-  registers[add_in1] = (Wr_ctrl) ? in : registers[add_in1];
-  registers[add_in2] = (Wr_ctrl) ? in : registers[add_in2];
+    if (!nrst)
+    begin
+      for (int i = 0; i < reg_num; i = i + 1)
+        registers[i] <= 16'h0;                          // clear register file
+    end
+        else
+      begin
+        registers[adrs_in1] <= (wr_ctrl ) ? in : registers[adrs_in1];
+        registers[adrs_in2] <= (wr_ctrl ) ? in : registers[adrs_in2];
+        out <= registers[adrs_out] ;  
+      end
   end
-endmodule
+  
+endmodule 
