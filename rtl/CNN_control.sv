00acfe1a3e2a7495b305c3ba46a2359e364742d1
Raneem Alaa <raneemalaa55@gmail.com>
2021-12-18 01:14:44 +0200

    minor changes

--- rtl/CNN_control.sv
+++ rtl/CNN_control.sv
@@ -1,7 +1,8 @@
 module ctrl_cnn(
   input  logic clk, nrst, conv_finish,pu_finish ,pooling_finish, [13:0]op_code_i,
   output logic pu_en,conv_ctrl,pooling_ctrl,
-  output logic [31:0] weight_en
+  output logic [31:0] weight_en,
+  output logic [5:0] weight_dim
 );
 
   enum logic [2:0] { idle             = 3'b000,
@@ -19,6 +20,8 @@ module ctrl_cnn(
   logic [6:0] current_count_img, next_count_img;
   logic [4:0] current_count_conv, next_count_conv;	
   
+  assign weight_dim = op_code_i[13:9];
+
   always_ff @(posedge clk or negedge nrst) 
 	begin
 	  if(!nrst) 
@@ -27,7 +30,7 @@ module ctrl_cnn(
 	    current_count_img  <= 0;
       current_count_conv <= 0;
       current_i          <= 0;
-	  current_state_2    <= loading_weight_element;  
+	    current_state_2    <= loading_weight_element;  
       current_i          <= 0;
       current_N          <= 1;
     end
@@ -37,7 +40,7 @@ module ctrl_cnn(
       current_count_img  <= next_count_img;
       current_count_conv <= next_count_conv;
       current_i          <= next_i;
-	  current_state_2    <= next_state_2;
+	    current_state_2    <= next_state_2;
       current_N          <= next_N ;
       current_i          <= next_i;
     end 
@@ -65,14 +68,14 @@ module ctrl_cnn(
           3'b001:next_state = conv;
           3'b011:next_state = FC;
           3'b010:next_state = conv_pooling;
-          3'b110:next_state =conv_pooling_FC;
-          3'b111:next_state =out;	
+          3'b110:next_state = conv_pooling_FC;
+          3'b111:next_state = out;	
 	      endcase	
       end
 
 		  conv:
 		  begin                             //filter buffering
-		  pooling_ctrl = 0;
+		    pooling_ctrl = 0;
        	if (next_i < op_code_i[8:3]) // to make first buffer for filter on 
        	begin 
 				  unique case(current_state_2) 
@@ -84,10 +87,11 @@ module ctrl_cnn(
          			begin 
           			weight_en[next_i-1] = 0; 
          			end
-             			 next_N       = current_N + 2;               //AXI sends 2 elements per clk 
+
+             	next_N       = current_N + 2;               //AXI sends 2 elements per clk 
        				if(next_N < op_code_i[13:9] - 1)         // no of element 
        				begin
-                		next_state_2 = loading_weight_element;
+                next_state_2 = loading_weight_element;
        				end 
        				else 
 							begin
@@ -99,7 +103,7 @@ module ctrl_cnn(
   					begin
  						  if(next_i < op_code_i[8:3])  // no of filter
    						begin
-                			weight_en [next_i] = 1;
+                weight_en [next_i] = 1;
  						    if(next_i != 0)
          			  begin 
           			  weight_en[next_i-1] = 0; 
@@ -115,14 +119,14 @@ module ctrl_cnn(
 			  else 
 				begin
 			    pu_en          = 1;
-          		weight_en      = '0;	
-          		next_state     = conv;
+          weight_en      = '0;	
+          next_state     = conv;
 			    next_count_img = current_count_img + 1;	
 				  if (next_count_img > 7'd67)
 				  begin
-            		conv_ctrl  = 1;
-            		pu_en      = (pu_finish)? 0 : 1;
-            		weight_en  = '0;
+            conv_ctrl  = 1;
+            pu_en      = (pu_finish)? 0 : 1;
+            weight_en  = '0;
 				    next_state = conv_finish? out : conv ;
           end
 				end
@@ -138,7 +142,7 @@ module ctrl_cnn(
 		  begin                             //filter buffering
        	if (next_i < op_code_i[8:3]) // to make first buffer for filter on 
        	begin 
-		   pooling_ctrl = 0;
+		      pooling_ctrl = 0;
 				  unique case(current_state_2) 
   				  loading_weight_element:
   					begin 
@@ -148,10 +152,11 @@ module ctrl_cnn(
          			begin 
           			weight_en[next_i-1] = 0; 
          			end
-              		next_N       = current_N + 2;               //AXI sends 2 elements per clk 
+
+              next_N       = current_N + 2;               //AXI sends 2 elements per clk 
        				if(next_N < op_code_i[13:9] - 1)         // no of element 
        				begin
-                	next_state_2 = loading_weight_element;
+                next_state_2 = loading_weight_element;
        				end 
        				else 
 							begin
@@ -163,7 +168,7 @@ module ctrl_cnn(
   					begin
  						  if(next_i < op_code_i[8:3])  // no of filter
    						begin
-               			 weight_en [next_i] = 1;
+               	weight_en [next_i] = 1;
  						    if(next_i != 0)
          			  begin 
           			  weight_en[next_i-1] = 0; 
@@ -183,10 +188,10 @@ module ctrl_cnn(
           weight_en      = 0;
           pooling_ctrl   = 0;
           conv_ctrl      = 0;	
-		  next_count_img = current_count_img + 1;
+		      next_count_img = current_count_img + 1;
           next_state     = conv_pooling;
 
-	      if ( next_count_img > 7'd67 ) 
+	        if ( next_count_img > 7'd67 ) 
 				  begin
             if (next_count_conv < op_code_i[13:9]) 
             begin
@@ -203,7 +208,7 @@ module ctrl_cnn(
               pooling_ctrl = 1;
               weight_en    = 0;
               conv_ctrl    = (conv_finish)? 0 : 1;
-			  next_state   = (pooling_finish)? out : conv_pooling ;
+			        next_state   = (pooling_finish)? out : conv_pooling ;
             end
           end 
 				end               
