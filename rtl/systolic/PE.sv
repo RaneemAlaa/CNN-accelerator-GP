4c1e44fff10daed413eb3575fa9a4ef0a2cdc21e
AhmedMaagdy <94821384+AhmedMaagdy@users.noreply.github.com>
2021-12-27 19:48:13 +0200

    Update PE.sv

--- rtl/systolic/PE.sv
+++ rtl/systolic/PE.sv
@@ -1,33 +1,26 @@
-module PE(clk_in,nrst_in,ctrl_in,PE_in,PE_out,feature_in,feature_out);
-parameter w = 8; // data_width
-	//input port
-input clk_in,nrst_in,ctrl_in;
-input [w-1:0]PE_in,feature_in;
-	//output port
-output [w-1:0] feature_out;
-output logic [w-1:0] PE_out;
-	//internal registers & wires
-logic [w-1:0] weight_in,partial_sum_in;
-logic [w*2-1:0] mul_out;
-logic [w-1:0]tmp,partial_sum_out ;
-	
-assign mul_out = weight_in * feature_in;
-assign tmp = mul_out[w-1:0];              	 //quantization
-assign partial_sum_out = partial_sum_in +tmp;
-assign feature_out = (~nrst_in)?0:feature_in;
-always @(posedge clk_in or negedge nrst_in )begin
-	if(!nrst_in) begin
-		PE_out <= 0;
-		partial_sum_in <=0;
-		weight_in <= 0;
-	end
-	else if(ctrl_in) begin
-		weight_in <= PE_in;
-		PE_out <= PE_in;
+module pe#(parameter width = 8) (input clk,rst_n,ctrl,
+	input [width-1:0]PE_in, feature_in,
+	output logic [width-1:0]PE_out,feature_out);
+logic [width-1:0]weight,tmp;
+logic [width*2-1:0]mul_out; 
+always_ff @(posedge clk or negedge rst_n) begin : proc_
+	if(~rst_n) begin
+		 PE_out <= 0;
+		 feature_out <= 0;
+		 weight <= 0;
+	end 
+	else if (ctrl) begin
+		 PE_out <= PE_in ;
+		 weight <= PE_in;
+		 feature_out <= 0;
 	end
 	else begin
-		partial_sum_in <= PE_in;
-		PE_out <= partial_sum_out;
+		feature_out <= feature_in;
+		PE_out <= PE_in + tmp; 
 	end
+end
+always_comb begin 
+	mul_out = weight * feature_in;
+	tmp = mul_out [width-1:0];	//quantization
 end
 endmodule
