c91b9f2b18fca9208b3cb17aa1ae8a0b88c34378
me98nna <82970046+me98nna@users.noreply.github.com>
2022-03-05 01:31:18 +0200

    add  input enable signal to systollic

--- rtl/systolic/conv_ctrl.sv
+++ rtl/systolic/conv_ctrl.sv
@@ -4,13 +4,16 @@ module conv_ctrl #(
     input  logic clk, nrst, conv_ctrl,
     input  logic [4:0] weight_dim,
     output logic conv_finish, w_ps, 
-    output logic [col-1:0] out_en
+    output logic [col-1:0] out_en,
+    output logic [31:0]input_en
   );
 
   enum logic [1:0] {  loading_weight = 2'b01,
                       loading_PS     = 2'b11 } current_state,next_state;
 
   logic[4:0] current_count, next_count;
+  logic[4:0] current_i,next_i; //!number of PEs
+  logic[5:0] next_clock_counter,current_clock_counter;//! signal detrmine when we will disenable the the systollic to take input 
   logic first_out; 
 
   always_ff @(posedge clk, negedge nrst) 
@@ -19,11 +22,15 @@ module conv_ctrl #(
     begin
       current_state <= loading_weight;
       current_count <= 4'b0;
+      current_i<='0;
+      current_clock_counter<='0;
     end
 		else
     begin
       current_state <= next_state;
       current_count <= next_count;
+      current_i<=next_i;
+      current_clock_counter<=next_clock_counter;
     end 
 	end
 
@@ -32,6 +39,8 @@ module conv_ctrl #(
 	begin 
 	  next_count = current_count;
 	  next_state = current_state;
+    next_i=current_i;
+    next_clock_counter=current_clock_counter;
 
 	  unique case(current_state) 
 			loading_weight: 
@@ -45,6 +54,12 @@ module conv_ctrl #(
       loading_PS:
       begin
         w_ps = 0;
+         next_clock_counter=next_clock_counter+1;
+         if (next_clock_counter > 28 && next_clock_counter <55)
+          begin 
+           input_en [next_i] = 1;
+ 					 next_i=current_i+1;
+          end   
         if ( (next_count < weight_dim) && !first_out ) begin
           next_state  = loading_PS ;
           next_count  = current_count + 1;
