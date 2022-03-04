123b543f1da657a6b9455f487dd8ee199417716d
AhmedMaagdy <94821384+AhmedMaagdy@users.noreply.github.com>
2022-03-04 14:47:03 +0200

    Update systolic_top.sv

--- rtl/systolic/systolic_top.sv
+++ rtl/systolic/systolic_top.sv
@@ -1,18 +1,19 @@
-module systolic_top #(parameter width = 8, col = 3, row = 3 ) ( 
-	input  logic clk, nrst,conv_ctrl,
+module systolic_top 
+	#(parameter width = 8, col = 4, row = 4 )
+	 ( 
+	input  logic clk, nrst,conv_ctrl,weight_en,in_en[row-1:0],
 	input  logic [4:0] weight_dim,
 	input  var logic  [width-1:0] weight_input2 [col-1:0] ,
 	input  var logic [width-1:0] feature_input2 [row-1:0],
 	output logic [width-1:0] systolic_out [col-1:0],
-	output logic conv_finish, w_ps, 
-    output logic [col-1:0] out_en
+	output logic out_en [col-1:0]
 	);
+logic w_ps;
 conv_ctrl #(.col(col))
 	ctrl_inst(.clk(clk),
 		.nrst(nrst),
 		.conv_ctrl(conv_ctrl),
 		.weight_dim(weight_dim),
-		.conv_finish(conv_finish),
 		.w_ps(w_ps),
 		.out_en(out_en)
 		);
@@ -22,6 +23,8 @@ systolic_array #(.width(width),
 	sys_inst(.clk_in2(clk),
 		.nrst_in2(nrst),
 		.ctrl_in2(w_ps),
+		.weight_en(weight_en),
+		.in_en(in_en),
 		.weight_input2(weight_input2),
 		.feature_input2(feature_input2),
 		.systolic_out(systolic_out)
