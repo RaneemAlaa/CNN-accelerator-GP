d97e55937db3e553903a51e851fce0917ba3e02e
AhmedMaagdy <94821384+AhmedMaagdy@users.noreply.github.com>
2022-03-04 14:45:27 +0200

    Update sys_vector.sv

--- rtl/systolic/sys_vector.sv
+++ rtl/systolic/sys_vector.sv
@@ -1,6 +1,7 @@
 module systolic_vector
-  #(parameter width=8 , row = 3)(
-  input clk_in1, nrst_in1, ctrl_in1,[width-1:0] weight_input1,[width-1:0] feature_input[row-1:0],
+  #(parameter width=8 , row = 4)(
+  input clk_in1, nrst_in1, ctrl_in1,weight_en,in_en[row-1:0],[width-1:0] weight_input1,[width-1:0] feature_input[row-1:0],
+  output logic out_in[row-1:0],
   output logic [width-1:0] vec_out , [width-1:0] feature_out1[row-1:0]
   );
 
@@ -8,17 +9,19 @@ module systolic_vector
 logic [width-1:0] weight[row:0];
 
 
-assign weight[0] = weight_input1;
+assign weight[0] = (weight_en)? weight_input1:'b0;
 
 assign vec_out=weight[row];
 genvar i;
 generate
     for (i=0; i<=row-1; i=i+1) begin 
-        pe
+        pe #(.width(width))
          p1(
         .clk(clk_in1),
         .rst_n(nrst_in1),
         .ctrl(ctrl_in1),
+        .in_en(in_en[i]),
+        .out_in(out_in[i]),
         .PE_in(weight[i]),
         .feature_in(feature_input[i]),
         .PE_out(weight[i+1]),
