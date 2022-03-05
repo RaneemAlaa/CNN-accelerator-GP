32dca0d1522bd3e7cc2e9d53ac25907d59c6356e
AhmedMaagdy <94821384+AhmedMaagdy@users.noreply.github.com>
2022-03-05 19:55:38 +0200

    Update systolic_array.sv

--- rtl/systolic/systolic_array.sv
+++ rtl/systolic/systolic_array.sv
@@ -1,6 +1,6 @@
 module systolic_array
   #(parameter width=8 , row=4 ,  col=4)(
-  input clk_in2, nrst_in2,ctrl_in2,weight_en,in_en [row-1:0],[width-1:0] weight_input2 [col-1:0] ,[width-1:0] feature_input2 [row-1:0],
+  input clk_in2, nrst_in2,ctrl_in2,weight_en[col-1:0],in_en [row-1:0],[width-1:0] weight_input2 [col-1:0] ,[width-1:0] feature_input2 [row-1:0],
   output [width-1:0] systolic_out [col-1:0]
   );
 
@@ -23,7 +23,7 @@ generate
         		.clk_in1(clk_in2),
         		.nrst_in1(nrst_in2),
        			.ctrl_in1(ctrl_in2),
-       			.weight_en(weight_en),
+			.weight_en(weight_en[j]),
        			.in_en(en_wires[(j+1)*row-1:j*row]),
        			.out_in(en_wires[(j+2)*row-1:(j+1)*row]),
         		.weight_input1(weight_input2[j]),
