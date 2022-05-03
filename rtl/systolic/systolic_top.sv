10b09b5d0d7e3ead5cacde723ae859906ec85d33
AhmedMaagdy <94821384+AhmedMaagdy@users.noreply.github.com>
2022-05-03 12:52:17 +0200

    Update systolic_top.sv

--- rtl/systolic/systolic_top.sv
+++ rtl/systolic/systolic_top.sv
@@ -2,23 +2,28 @@ module systolic_top
 	#(parameter width = 16, col = 32, row = 32 )
 	 ( 
 	input  logic clk, nrst,conv_ctrl,
-	input logic [col-1:0] weight_en,
+	input  logic [col-1:0] weight_en,
 	input  logic [4:0] weight_dim,
 	input  var logic  [width-1:0] weight_input2 [col-1:0] ,
 	input  var logic [width-1:0] feature_input2 [row-1:0],
+	input  logic [4:0] num_filter,
+	output logic  out_en[col-1:0],
 	output logic [width-1:0] systolic_out [col-1:0],
 	output logic conv_finish
 	);
 logic w_ps;
 logic [row-1:0] in_en;
-conv_ctrl #(.col(col))
+conv_ctrl #(.col(col),
+	.row(row))
 	ctrl_inst(.clk(clk),
 		.nrst(nrst),
 		.conv_ctrl(conv_ctrl),
 		.input_en(in_en),
 		.weight_dim(weight_dim),
+		.num_filter (num_filter),
+		.out_en(out_en),
 		.w_ps(w_ps),
-		.conv_finish(conv_finish) 
+		.conv_finish(conv_finish)
 		);
 systolic_array #(.width(width),
 	.col(col),
