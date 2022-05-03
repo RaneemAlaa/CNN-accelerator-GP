7c1da9df0668fb47d407f8230624c2a271db9aa8
AhmedMaagdy <94821384+AhmedMaagdy@users.noreply.github.com>
2022-05-03 12:11:02 +0200

    Update regfile.sv

--- rtl/Pooling/regfile.sv
+++ rtl/Pooling/regfile.sv
@@ -12,20 +12,15 @@ module regfilePooling#(
 
 logic [data_width-1:0] registers [reg_num-1:0];
 
-always_ff @ (negedge clk, negedge nrst) 
-  begin
-    if (!nrst)
-    begin
+always_latch begin 
+    if (!nrst) begin
         registers <= '{default:0};                          // clear register file
     end
-        else
-      begin
+    else if (!clk) begin
         registers[adrs_in] = (wr_ctrl2 ) ? in2 : registers[adrs_in];
         registers[4'hf] = (wr_ctrl1 ) ? in1 : registers[4'hf];
         out <= registers[adrs_out] ;   
-      end
+    end
   end
-
-
 
 endmodule
